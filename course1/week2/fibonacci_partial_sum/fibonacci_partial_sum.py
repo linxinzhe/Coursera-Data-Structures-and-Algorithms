b000f48c2ea2687afc13da43a12d29ce93504edc
linxinzhe <linxinzhechn@icloud.com>
2017-06-17 10:34:35 +0800

    feat(week2): fibonacci partial sum

--- course1/week2/fibonacci_partial_sum/fibonacci_partial_sum.py
+++ course1/week2/fibonacci_partial_sum/fibonacci_partial_sum.py
@@ -1,11 +1,12 @@
 # Uses python3
 import sys
 
+
 def fibonacci_partial_sum_naive(from_, to):
     sum = 0
 
     current = 0
-    next  = 1
+    next = 1
 
     for i in range(to + 1):
         if i >= from_:
@@ -16,7 +17,22 @@ def fibonacci_partial_sum_naive(from_, to):
     return sum % 10
 
 
+def fibonacci_partial_sum(from_, to):
+    sum = 0
+
+    current = 0
+    next = 1
+
+    for i in range(to + 1):
+        if i >= from_:
+            sum += current % 10
+
+        current, next = next, (current + next) % 10
+
+    return sum % 10
+
+
 if __name__ == '__main__':
     input = sys.stdin.read()
     from_, to = map(int, input.split())
-    print(fibonacci_partial_sum_naive(from_, to))
+    print(fibonacci_partial_sum(from_, to))
