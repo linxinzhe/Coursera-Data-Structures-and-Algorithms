911d9c30004c4fb2ec40ed1777c68c5254307962
linxinzhe <linxinzhechn@icloud.com>
2017-06-17 10:31:37 +0800

    feat(week2): sum last digit

--- course1/week2/fibonacci_sum_last_digit/fibonacci_sum_last_digit.py
+++ course1/week2/fibonacci_sum_last_digit/fibonacci_sum_last_digit.py
@@ -1,13 +1,14 @@
 # Uses python3
 import sys
 
+
 def fibonacci_sum_naive(n):
     if n <= 1:
         return n
 
     previous = 0
-    current  = 1
-    sum      = 1
+    current = 1
+    sum = 1
 
     for _ in range(n - 1):
         previous, current = current, previous + current
@@ -15,7 +16,23 @@ def fibonacci_sum_naive(n):
 
     return sum % 10
 
+
+def fibonacci_sum(n):
+    if n <= 1:
+        return n
+
+    previous = 0
+    current = 1
+    sum = 1
+
+    for _ in range(n - 1):
+        previous, current = current, (previous + current) % 10
+        sum += current
+
+    return sum % 10
+
+
 if __name__ == '__main__':
     input = sys.stdin.read()
     n = int(input)
-    print(fibonacci_sum_naive(n))
+    print(fibonacci_sum(n))
